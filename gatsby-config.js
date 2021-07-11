185a67435af830b5c4c3b4ea97a01a25cb989fb3
dsgreve <dale@dalegreve.com>
2021-07-11 14:04:41 -0400

    adding images to file system

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
     },
     "gatsby-plugin-postcss",
     "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
     {
       resolve: "gatsby-plugin-google-analytics",
       options: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
@@ -7,6 +8,7 @@ const IndexPage = () => {
       <Layout pageTitle="Home Page">
         <h2>Welcome to my Gatsby site!</h2>
         <p>Built following the Gatsby Tutorial!</p>
+        <StaticImage alt="Comic panels of man buying hpnosis book" src="https://pbs.twimg.com/media/E6AbLx5VUAQCiUk?format=jpg&name=large" />
       </Layout>
       
     </main>
