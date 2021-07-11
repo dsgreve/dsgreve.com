4e97d87e5ef55f83d771abd318125a05b633a4d0
dsgreve <dale@dalegreve.com>
2021-07-11 12:40:47 -0400

    finished part 2 of gatsby build

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+// Import react component
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <main>
+      <Layout pageTitle="About dsgreve">
+        <p>Hit there I'm the proud creator of this site, which I built with Gatsby.</p>
+      </Layout>
+    </main>
+  )
+}
+
+export default AboutPage
