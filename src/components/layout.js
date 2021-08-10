5f5bd6f397f34ad2787d1e1e05bf36bbdda39967
dsgreve <dale@dalegreve.com>
2021-08-09 22:20:04 -0400

    switched site to gatsby wordpress blog starter

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,37 +1,47 @@
-import * as React from 'react'
-import { Link, useStaticQuery, graphql } from 'gatsby'
-import { 
-  container,
-  heading,
-  navLinks,
-  navLinkItem,
-  navLinkText,
-  siteTitle,
-} from './layout.module.css'
+import React from "react"
+import { Link, useStaticQuery, graphql } from "gatsby"
+import parse from "html-react-parser"
 
-const Layout = ({pageTitle, children}) => {
-  const data = useStaticQuery(graphql`
-  query {
-    site {
-      siteMetadata {
-        title
+const Layout = ({ isHomePage, children }) => {
+  const {
+    wp: {
+      generalSettings: { title },
+    },
+  } = useStaticQuery(graphql`
+    query LayoutQuery {
+      wp {
+        generalSettings {
+          title
+          description
+        }
       }
     }
-  }
   `)
+
   return (
-    <main className={container}>
-      <title>{pageTitle} | {data.site.siteMetadata.title}</title>
-      <p className={siteTitle}>{data.site.siteMetadata.title}</p>
-        <nav>
-          <ul className={navLinks}>
-            <li className={navLinkItem}><Link to = "/" className={navLinkText}>Home</Link></li>
-            <li><Link to = "/about" className={navLinkText}>About</Link></li>
-          </ul>
-        </nav>
-        <h1 className={heading}>{pageTitle}</h1>
-        {children}
-    </main>
+    <div className="global-wrapper" data-is-root-path={isHomePage}>
+      <header className="global-header">
+        {isHomePage ? (
+          <h1 className="main-heading">
+            <Link to="/">{parse(title)}</Link>
+          </h1>
+        ) : (
+          <Link className="header-link-home" to="/">
+            {title}
+          </Link>
+        )}
+      </header>
+
+      <main>{children}</main>
+
+      <footer>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.com">Gatsby</a>
+        {` `}
+        And <a href="https://wordpress.org/">WordPress</a>
+      </footer>
+    </div>
   )
 }
 
