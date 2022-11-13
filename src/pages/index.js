abbb62cd5e6c57b6e96f25df8b6819c4e8edec67
dsgreve <dale@dalegreve.com>
2022-11-13 10:19:13 -0500

    added image add css effect

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,27 @@
 import * as React from "react";
 import Intro from "../components/intro";
+import { StaticImage } from "gatsby-plugin-image";
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Intro pageTitle="Dale Greve">
-      <h2 className="text-3xl text-white">Creativity Through Code</h2>
+      <h2 className="text-3xl text-white mb-6">Creativity Through Code</h2>
+      <StaticImage
+        className="border-4 border-white rounded-2xl image-bounce"
+        alt="Example code"
+        src="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=280&q=80"
+      />
     </Intro>
   );
 };
 
 // You'll learn about this in the next task, just copy it for now
-export const Head = () => <title>DSG | Web Artisan</title>;
+export const Head = () => (
+  <>
+    <title>About DSG | Web Artisans</title>
+    <meta name="description" content="Personal portfolio site of Dale Greve" />
+  </>
+);
 
 // Step 3: Export your component
 export default IndexPage;
